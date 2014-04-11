6fb54c456ce2a6825749782f6f0d643b6f99a1a5
Patrick Coffey <patrickcoffey48@gmail.com>
2014-04-10 23:00:28 -0500

    generate random alphanumeric string

--- generate.php
+++ generate.php
@@ -1,48 +1,12 @@
 <?php
-  // Pulling _POST data
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $for = $_POST['item'];
-
-  // Generating keywords
-  $keywords = array('excacerbate', 'diehard', 'trustworthy', 'complicated', 'encrypted', 'corrupted', 'amalgamate', 'rhetorical', 'necessary', 'unwilling', 'impatient');
-  $symbols = array('!', '@', '#', '$','%', '^', '&', '*', '(', ')');
-  $hashed = array();
-  $which = rand(0, sizeOf($keywords));
-  $keyword = $keyword[$which];
-  $hash = md5($keyword);
-  $data = array(
-    'name' => str_split($name),
-    'email' => str_split($email),
-    'for' => str_split($for),
-  );
-
-  // Hashing process
-  // Starting with the keywords
-  $split = str_split($hash);
-  for($i = 0; $i < rand(5, 10); $i++) {
-    $which = rand(0, sizeOf($split));
-    $md5_hash .= $split[$which];
+function generateRandomString($len = 10) {
+  $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  $rand = ''; 
+  for ($i = 0; $i < $len; $i++) {
+    $rand .= $chars[rand(0, strlen($chars) - 1)];
   }
+  return $rand;
+}
 
-  // Moving to the provided info
-  for($i = 0; $i < rand(2, 4); $i++) {
-    foreach($data as $key => $value) {
-      $which = rand(0, sizeOf($value));
-      $str_hash .= $value[$offset];
-    }
-  }
-
-  // And now the symbols
-  for($i = 0; $i < rand(3, 5); $i++) {
-    $which = rand(0, sizeOf($symbols));
-    $sym_hash .= $symbols[$which];
-  }
-
-  $whichOnes = array('no', 'no', 'no');
-
-  // Now to put it all together
-  $hash = $md5_hash . $sym_hash . $str_hash;
-
-  echo json_encode(array('result' => 'worked', 'password' => $hash,));
-?>
+$pass = generateRandomString(10);
+echo json_encode(array('result' => 'worked', 'password' => $pass));                                                                                                               
